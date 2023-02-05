8e1f660ca2548d90f32910d856a3c05c14d73baf
Ben Follington <5009316+bfollington@users.noreply.github.com>
2023-02-05 11:02:17 +1000

    Compute & cache embeddings for notes & use for searching

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import * as dotenv from "dotenv";
 import * as fs from "fs";
 import { ChatGPTAPI } from "chatgpt";
+import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 async function ask(query) {
   const api = new ChatGPTAPI({
@@ -13,11 +18,19 @@ async function ask(query) {
   console.log(res.text);
 }
 
-// example();
+function getAllNoteFilenames() {
+  return fs.readdirSync("./notes").filter((f) => f.endsWith(".subtext"));
+}
 
 function getAllNoteTitles() {
-  const files = fs.readdirSync("./notes");
+  const files = getAllNoteFilenames();
   return files.map((f) => f.replace(".subtext", ""));
+}
+
+function readNoteBody(filename) {
+  const text = fs.readFileSync(`./notes/${filename}`, "utf-8");
+  const note = processNote(text);
+  return note;
 }
 
 function extendTitles() {
@@ -52,12 +65,9 @@ function processNote(note) {
 
 function rewordNote() {
   // select random file in folder
-  const files = fs.readdirSync("./notes");
+  const files = getAllNoteFilenames();
   const randomFile = files[Math.floor(Math.random() * files.length)];
-
-  // read file content
-  const text = fs.readFileSync(`./notes/${randomFile}`, "utf-8");
-  const note = processNote(text);
+  const note = readNoteBody(randomFile);
 
   if (note.length < 5) {
     console.warn("Note too short, skipping");
@@ -100,12 +110,9 @@ function howAreTheseRelated() {
 
 function addSlashlinks() {
   // select random file in folder
-  const files = fs.readdirSync("./notes");
+  const files = getAllNoteFilenames();
   const randomFile = files[Math.floor(Math.random() * files.length)];
-
-  // read file content
-  const text = fs.readFileSync(`./notes/${randomFile}`, "utf-8");
-  const note = processNote(text);
+  const note = readNoteBody(randomFile);
 
   if (note.length < 5) {
     console.warn("Note too short, skipping");
@@ -127,16 +134,13 @@ function addSlashlinks() {
 
 function connect() {
   // select random file in folder
-  const files = fs.readdirSync("./notes");
+  const files = getAllNoteFilenames();
   const randomFile = files[Math.floor(Math.random() * files.length)];
   const randomFile2 = files[Math.floor(Math.random() * files.length)];
 
   // read file content
-  const text = fs.readFileSync(`./notes/${randomFile}`, "utf-8");
-  const note = processNote(text);
-
-  const text2 = fs.readFileSync(`./notes/${randomFile2}`, "utf-8");
-  const note2 = processNote(text2);
+  const note = readNoteBody(randomFile);
+  const note2 = readNoteBody(randomFile2);
 
   if (note.length < 5 || note2.length < 5) {
     console.warn("Note too short, skipping");
@@ -161,12 +165,9 @@ function connect() {
 
 function extendNote() {
   // select random file in folder
-  const files = fs.readdirSync("./notes");
+  const files = getAllNoteFilenames();
   const randomFile = files[Math.floor(Math.random() * files.length)];
-
-  // read file content
-  const text = fs.readFileSync(`./notes/${randomFile}`, "utf-8");
-  const note = processNote(text);
+  const note = readNoteBody(randomFile);
 
   if (note.length < 5) {
     console.warn("Note too short, skipping");
@@ -186,4 +187,102 @@ function extendNote() {
   ask(query);
 }
 
-extendNote();
+async function getEmbedding(text) {
+  const res = await openai.createEmbedding({
+    model: "text-embedding-ada-002",
+    input: text,
+  });
+
+  const embedding = res.data.data.find((o) => o.object === "embedding");
+  return embedding;
+}
+
+function cosinesim(A, B) {
+  let dotproduct = 0;
+  let mA = 0;
+  let mB = 0;
+  for (let i = 0; i < A.length; i++) {
+    // here you missed the i++
+    dotproduct += A[i] * B[i];
+    mA += A[i] * A[i];
+    mB += B[i] * B[i];
+  }
+  mA = Math.sqrt(mA);
+  mB = Math.sqrt(mB);
+  let similarity = dotproduct / (mA * mB); // here you needed extra brackets
+  return similarity;
+}
+
+async function getEmbeddingForRandomNoteTitle() {
+  // select random file in folder
+  const files = getAllNoteFilenames();
+  const randomFile = files[Math.floor(Math.random() * files.length)];
+  const processedName = randomFile.replace(".subtext", "").replace(/-/g, " ");
+
+  console.log(processedName);
+  return await getEmbedding(processedName);
+}
+
+// const a = await getEmbeddingForRandomNoteTitle();
+// const b = await getEmbeddingForRandomNoteTitle();
+// console.log(cosinesim(a.embedding, b.embedding));
+
+async function getEmbeddings(max) {
+  let files = fs.readdirSync("./notes").filter((f) => f.endsWith(".subtext"));
+  const cache = loadCachedEmbeddings();
+
+  files = files.slice(0, max);
+
+  for (let f of files) {
+    if (cache[f]) continue;
+
+    const processedName = f.replace(".subtext", "").replace(/-/g, " ");
+    const text = fs.readFileSync(`./notes/${f}`, "utf-8");
+    const note = processNote(text);
+    const combined = `${processedName}\n\n${note}`;
+    console.log(combined);
+
+    const embedding = await getEmbedding(combined);
+
+    cache[f] = embedding;
+  }
+
+  const json = JSON.stringify(cache);
+  // save to disk
+  console.log(json);
+  fs.writeFileSync("./embeddings.json", json);
+}
+
+function loadCachedEmbeddings() {
+  try {
+    const data = fs.readFileSync("./embeddings.json");
+    return JSON.parse(data);
+  } catch {
+    return {};
+  }
+}
+
+async function findRelatedNotes(text, count = 10, threshold = 0.75) {
+  const cache = loadCachedEmbeddings();
+  const embedding = await getEmbedding(text);
+  let related = [];
+
+  for (let f in cache) {
+    const similarity = cosinesim(embedding.embedding, cache[f].embedding);
+    if (similarity > threshold) {
+      related.push({ file: f, similarity });
+    }
+  }
+
+  related.sort((a, b) => b.similarity - a.similarity);
+  related = related.slice(0, count);
+
+  console.log(related);
+  return related;
+}
+
+// getEmbeddings(135);
+
+// get cli args
+const args = process.argv.slice(2);
+findRelatedNotes(args.join(" "), 5, 0.75);
